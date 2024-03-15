fdac0e39ecd5e30b04d180ff70c91cbb028050a9
zo1229 <alpwilliams@my365.bellevue.edu>
2024-03-15 01:25:54 -0400

    Assigned Upload.

---
+++ Module_1/HelloWorldApp/WilliamsHelloWorld.java
@@ -0,0 +1,13 @@
+package Module_1.HelloWorldApp;
+/*Williams, A., (2024).  CIS 505 Intermediate Java Programming.  Bellevue University.*/
+
+class WilliamsHelloWorld
+{
+    public static void main(String[] args) 
+    {
+        String firstName = "Alphonso";
+        String lastName = "Williams";
+               
+        System.out.printf("Hello World from %s.%n", firstName + " " + lastName);
+    }
+}
