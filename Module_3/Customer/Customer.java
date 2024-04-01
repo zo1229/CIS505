343df7b4fc1c8e14c275341a57f4ce3152e563e6
zo1229 <alpwilliams@my365.bellevue.edu>
2024-03-31 23:53:53 -0400

    “Module_3

---
+++ Module_3/Customer/Customer.java
@@ -0,0 +1,82 @@
+package Module_3.Customer;
+
+class Customer {
+    private String name;
+    private String address;
+    private String city;
+    private String zip;
+
+    public Customer() {
+
+    }
+
+    public Customer(String name, String address, String city, String zip) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.zip = zip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nAddress: " + address + "\nCity: " + city + "\nZip: " + zip;
+    }
+}
+
+class CustomerDB {
+    public static Customer getCustomer(Integer id) {
+        Customer customer;
+        if (id == 1007) {
+            customer = new Customer("Michael McGee", "123 Professor St", "Anytown", "98788");
+        } else if (id == 1008) {
+            customer = new Customer("Alphonso Williams", "456 Honor Roll Blvd", "Sometown", "87890");
+        } else if (id == 1009) {
+            customer = new Customer("Puff Daddy", "789 Prison Rd", "New York", "89890");
+        } else {
+            customer = new Customer("Unknown", "Unknown", "Unknown", "Unknown");
+        }
+        return customer;
+    }
+}
+
+class Account {
+    private double balance = 200;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amt) {
+        balance += amt;
+    }
+
+    public void withdraw(double amt) {
+        if (balance >= amt) {
+            balance -= amt;
+        }
+    }
+
+    public void displayMenu() {
+        System.out.println("Account Menu:");
+        System.out.println("1. Deposit");
+        System.out.println("2. Withdraw");
+        System.out.println("3. Exit");
+    }
+
+}
---
+++ Module_3/Customer/TestCustomerAccountApp.java
@@ -0,0 +1,47 @@
+package Module_3.Customer;
+
+import java.util.Scanner;
+
+public class TestCustomerAccountApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a customer number between 1007 and 1009:");
+        int customerId = scanner.nextInt();
+        Customer customer = CustomerDB.getCustomer(customerId);
+
+        System.out.println(customer);
+
+        Account account = new Account();
+
+        int choice;
+        do {
+            account.displayMenu();
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter deposit amount: ");
+                    double depositAmt = scanner.nextDouble();
+                    account.deposit(depositAmt);
+                    break;
+                case 2:
+                    System.out.print("Enter withdrawal amount: ");
+                    double withdrawAmt = scanner.nextDouble();
+                    account.withdraw(withdrawAmt);
+                    break;
+                case 3:
+                    System.out.println("Exiting menu.");
+                    break;
+                default:
+                    System.out.println("Error: Invalid Option.");
+                    break;
+            }
+        } while (choice != 3);
+
+        System.out.printf("Account Balance: $%,6.2f", account.getBalance());
+
+        scanner.close();
+    }
+}
